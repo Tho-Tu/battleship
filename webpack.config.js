9e4379b511309e92ad2e2764b4b2124d2627de36
Tho Tu <126043463+Tho-Tu@users.noreply.github.com>
2023-10-19 20:30:17 +1000

    Update webpack.config

--- webpack.config.js
+++ webpack.config.js
@@ -21,12 +21,16 @@ module.exports = {
     path: path.resolve(__dirname, "dist"),
     clean: true,
   },
-
-  moduleFileExtensions: ["js", "jsx"],
-  moduleDirectories: ["node_modules", "bower_components", "shared"],
-  moduleNameMapper: {
-    "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$":
-      "<rootDir>/__mocks__/fileMock.js",
-    "\\.(css|less)$": "<rootDir>/__mocks__/styleMock.js",
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
+      },
+    ],
   },
 };
