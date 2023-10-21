d0a06030da7575f273cf42b5878f649d8ec76ba6
Tho Tu <126043463+Tho-Tu@users.noreply.github.com>
2023-10-21 16:40:40 +1000

    Add placeship and verify placement for gameboard

--- src/gameboard.js
+++ src/gameboard.js
@@ -1,4 +1,66 @@
-export default function gameboard() {
-  let test;
-  return {};
+import ship from "./ship";
+
+export default function gameBoard() {
+  let gameGrid = [...Array(10)].map(() => Array(10));
+
+  //   define ships
+  let shipOne = ship(2);
+  let shipTwo = ship(3);
+  let shipThree = ship(4);
+  let shipFour = ship(5);
+  let allShips = [shipOne, shipTwo, shipThree, shipFour];
+
+  const getGameGrid = () => {
+    return gameGrid;
+  };
+
+  const verifyPosition = (x, y, ship, direction) => {
+    // verify that no other ship is at position and ship fits into grid
+    const gridSize = 10;
+    if (direction === "v") {
+      for (let i = 0; i < ship.shipLength; i++) {
+        if (gameGrid[y + i][x] !== undefined) {
+          console.log("another ship in this position");
+          return false;
+        }
+      }
+      return (
+        x >= 0 && x < gridSize && y >= 0 && y + ship.shipLength <= gridSize
+      );
+    } else if (direction === "h") {
+      for (let i = 0; i < ship.shipLength; i++) {
+        if (gameGrid[y][x + i] !== undefined) {
+          console.log("another ship in this position");
+          return false;
+        }
+      }
+      return (
+        x >= 0 && x + ship.shipLength <= gridSize && y >= 0 && y < gridSize
+      );
+    } else {
+      return false;
+    }
+  };
+
+  const placeShip = (x, y, ship, direction) => {
+    if (verifyPosition(x, y, ship, direction)) {
+      if (direction === "v") {
+        for (let i = 0; i < ship.shipLength; i++) {
+          gameGrid[y + i][x] = ship;
+        }
+      }
+
+      if (direction === "h") {
+        for (let i = 0; i < ship.shipLength; i++) {
+          gameGrid[y][x + i] = ship;
+        }
+      }
+    } else {
+      return false;
+    }
+  };
+
+  const receiveAttack = (x, y) => {};
+
+  return { getGameGrid, placeShip, receiveAttack };
 }
--- src/tests/gameboard.test.js
+++ src/tests/gameboard.test.js
@@ -1,1 +1,77 @@
-import gameboard from "../gameboard";
+import gameBoard from "../gameboard";
+import ship from "../ship";
+
+let exampleGameBoard;
+let exampleShip;
+let exampleShipTwo;
+let exampleShipThree;
+
+beforeEach(() => {
+  exampleGameBoard = gameBoard();
+  exampleShip = ship(4);
+  exampleGameBoard.placeShip(1, 1, exampleShip, "v");
+
+  exampleShipTwo = ship(2);
+  exampleGameBoard.placeShip(3, 0, exampleShipTwo, "h");
+});
+
+describe("testing vertical placement", () => {
+  let grid;
+  beforeEach(() => {
+    grid = exampleGameBoard.getGameGrid();
+  });
+  test("place ship", () => {
+    expect(grid[0][1]).toBeUndefined();
+  });
+  test("vertical position 1,1", () => {
+    expect(grid[1][1]).toBe(exampleShip);
+  });
+  test("vertical position 2,1", () => {
+    expect(grid[2][1]).toBe(exampleShip);
+  });
+  test("vertical position 3,1", () => {
+    expect(grid[3][1]).toBe(exampleShip);
+  });
+  test("vertical position 4,1", () => {
+    expect(grid[4][1]).toBe(exampleShip);
+  });
+});
+
+describe("test horizontal placement", () => {
+  let grid;
+  beforeEach(() => {
+    grid = exampleGameBoard.getGameGrid();
+  });
+  test("horizontal position 0,3", () => {
+    expect(grid[0][3]).toBe(exampleShipTwo);
+  });
+  test("horizontal position 0,4", () => {
+    expect(grid[0][4]).toBe(exampleShipTwo);
+  });
+});
+
+describe("test incorrect ship placement", () => {
+  let grid;
+  beforeEach(() => {
+    grid = exampleGameBoard.getGameGrid();
+    exampleShipThree = ship(3);
+  });
+
+  test("incorrect ship placement", () => {
+    expect(exampleGameBoard.placeShip(1, 0, exampleShipThree, "h")).toBe(false);
+  });
+  test("incorrect ship placement", () => {
+    expect(exampleGameBoard.placeShip(0, 4, exampleShipThree, "h")).toBe(false);
+  });
+});
+
+describe("attack ship", () => {
+  let grid;
+  beforeEach(() => {
+    grid = exampleGameBoard.getGameGrid();
+    exampleGameBoard.receiveAttack(3, 0);
+  });
+  test("attack ship two", () => {
+    expect(grid);
+  });
+});
